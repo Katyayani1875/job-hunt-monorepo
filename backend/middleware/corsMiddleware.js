54ba7cf6f71dfe7e347c07467c0b179f9d0d711a
Gurusewak Singh <150012557+gurusewak-singh@users.noreply.github.com>
2025-06-30 16:27:12 +0530

    Update corsMiddleware.js
    Added vercel frontend https://jobhunt-frontend-two.vercel.app/

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const allowedOrigins = [
-  process.env.FRONTEND_URL || 'http://localhost:5174','http://localhost:5173'
+  process.env.FRONTEND_URL || 'https://jobhunt-frontend-two.vercel.app/'
 ];
 
 const corsMiddleware = cors({
